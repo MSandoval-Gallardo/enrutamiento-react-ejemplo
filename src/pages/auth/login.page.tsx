d510b1b8c94a15a795290f4a4edbd83abeb940d3
MSandoval-Gallardo <m.sandovalgallardo@gmail.com>
2025-10-29 10:30:54 -0300

    instalación de tailwindcss y shadcn

--- src/pages/auth/login.page.tsx
+++ src/pages/auth/login.page.tsx
@@ -1,14 +1,39 @@
 import { Button } from "@/components/ui/button"
 import { useAuthActions } from "../../hooks/use-auth-actions"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { toast } from "sonner"
 
 const LoginPage = () => {
 
   const {loginWhitGoogle} = useAuthActions()
+
+  const handleLoginWithGoogle = async () => {
+      toast.error("Falló el inicio de sesion")
+    
+    const result = await loginWhitGoogle()
+    if(result.success){
+      console.log("Logeo con exito")
+    }else{
+      console.log("logeo fallido:", result.error)
+      toast.error("Falló el inicio de sesion")
+    }
+  }
   return (
-    <div>
-      <h1>Login</h1>
-      <Button onClick={loginWhitGoogle}>iniciar sesion con google</Button>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>Inicia sesion con email y contraseña o con Google</CardDescription>
+      </CardHeader>
+      <CardContent>...</CardContent>
+      <CardFooter>
+        <Button 
+          onClick={handleLoginWithGoogle}
+          className="w-full"
+        >
+          Inicio sesion con Google
+        </Button>
+      </CardFooter>
+    </Card>
   )
 }
 export default LoginPage
